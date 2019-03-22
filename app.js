cc7680de7494be68f07aef6b0557468236246f04
Jason Hoffman <j@sonhoffman.com>
2019-03-22 12:32:40 -0400

    remove auth code

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var authToken = "3a4c70d14ef4dd3a3ddb2fe567b38b68";
-var accountSid = "AC7a5910c2e815f67cd98c43ad4b8bee65";
+var authToken = "";
+var accountSid = "";
 
 var express = require('express');
 var app = express();
